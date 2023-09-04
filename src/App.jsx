07602f72424697085945861c3e17221843cb7aa4
Kanishka Sewwandi <109033047+Kanishk9907@users.noreply.github.com>
2023-09-04 18:02:06 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+
+const App = () =>{
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-semibold">Hello, Tailwind CSS!</h1>
+      <p className="text-lg">This is a React app with Tailwind CSS.</p>
+    </div>
+  );
+}
+
+export default App;
